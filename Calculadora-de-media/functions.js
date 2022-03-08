448d2ba7334184eb4a7a2f2a86089a7d13d2241b
VLGallo <vanessaleticiagallo@gmail.com>
2022-03-08 09:30:25 -0300

    aula 1

---
+++ Calculadora-de-media/functions.js
@@ -0,0 +1,17 @@
+var notaPrimeiroBimestre = 7;
+var notaSegundoBimestre = 4;
+var notaTerceiroBimestre = 3;
+var notaQuartoBimestre = 8;
+var media = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre)/4;
+var resultado = "";
+
+if (media >= 5){
+    resultado = "O aluno foi aprovado com " + media.toFixed(1);
+} else {
+   resultado = "O aluno foi reprovado com " + media.toFixed(1);
+}
+
+const exibirMedia = document.querySelector(".media");
+const p = document.createElement("p");
+p.innerText = resultado;
+div.media.appendChild(p);
