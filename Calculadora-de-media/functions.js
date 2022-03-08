28da9e2f510dccd7ac25e4432d5dde3d464c8486
VLGallo <vanessaleticiagallo@gmail.com>
2022-03-08 12:51:18 -0300

    refatorando aula 1

--- Calculadora-de-media/functions.js
+++ Calculadora-de-media/functions.js
@@ -1,14 +1,14 @@
-var notaPrimeiroBimestre = 7;
-var notaSegundoBimestre = 4;
-var notaTerceiroBimestre = 3;
-var notaQuartoBimestre = 8;
-var media = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre)/4;
+var notaPrimeiroBimestre = parseFloat(prompt("Digite a nota do primeiro semestre"));
+var notaSegundoBimestre = parseFloat(prompt("Digite a nota do segundo semestre"));
+var notaTerceiroBimestre = parseFloat(prompt("Digite a nota do terceiro semestre"));
+var notaQuartoBimestre = parseFloat(prompt("Digite a nota do quarto semestre"));
+var media = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4;
 var resultado = "";
 
-if (media >= 5){
+if (media >= 5) {
     resultado = "O aluno foi aprovado com " + media.toFixed(1);
 } else {
-   resultado = "O aluno foi reprovado com " + media.toFixed(1);
+    resultado = "O aluno foi reprovado com " + media.toFixed(1);
 }
 
 const exibirMedia = document.querySelector(".media");
