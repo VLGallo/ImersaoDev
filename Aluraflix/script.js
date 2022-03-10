3c9ac44c91fd970d977918d94912b3088f0891c0
VLGallo <vanessaleticiagallo@gmail.com>
2022-03-10 12:46:22 -0300

    Aula 4

---
+++ Aluraflix/script.js
@@ -0,0 +1,9 @@
+var filmes = ["https://vortexcultural.com.br/images/2021/05/milagre-na-cela-7.jpg",
+"https://leiturafilmica.com.br/wp-content/uploads/2021/05/mucize-poster.jpg",
+"https://br.web.img3.acsta.net/medias/nmedia/18/92/29/05/20190815.jpg",
+ "https://br.web.img2.acsta.net/medias/nmedia/18/86/96/34/20028591.jpg",
+"https://blog.facens.br/wp-content/uploads/2021/10/Lion-Uma-Jornada-para-Casa.jpg"];
+
+for (var i = 0; i < filmes.length; i++){
+document.write("<img src=" + filmes[i] +  ">")
+}
